5bfebc6120489250f742c71327b0b4c77843187a
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-15 16:06:35 -0700

    Added puts

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -50,8 +50,19 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-
-
+  Category.update({
+    category_name: req.body.category_name,
+  },
+  {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((data) => res.json(data))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
