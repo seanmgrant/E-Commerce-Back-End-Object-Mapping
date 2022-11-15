6dff62c0f0be8ff7ff66cf79133110170aded6f5
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-15 16:08:37 -0700

    Added delete to catagory router

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -67,8 +67,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-
-
+  Category.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((data) => {
+    res.status(200).json(data);
+  })
+  .catch((err) => {
+  console.log(err);
+  res.status(404).json(err);
+})
 });
 
 module.exports = router;
