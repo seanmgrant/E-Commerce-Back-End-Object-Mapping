1e1fb9f6c15864a35973f9fcdb6409df99a63ee0
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-15 15:54:09 -0700

    added more to tag router

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -23,10 +23,36 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product
+    }
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{ model: Product }],
+  })
+  .then((data) => {
+    res.status(200).json(data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(404).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
